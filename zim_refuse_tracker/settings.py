c89db6c98156d9d848f3a61eb64a117d5f8fb407
Brandon <Apex2.0predator@gmail.com>
2025-10-05 18:45:22 +0200

    added redis

--- zim_refuse_tracker/settings.py
+++ zim_refuse_tracker/settings.py
@@ -23,7 +23,7 @@ EMAIL_HOST_PASSWORD = "@Brandon40%"
 DEFAULT_FROM_EMAIL = 'brandkhumz40@gmail.com'
 
 
-REDIS_URL = config("REDIS_URL")
+REDIS_URL = config("REDIS_URL", deafult='redis://default:uQpFYAMIbKbTvRGoHTnxDjuWkjmHMSoF@redis.railway.internal:6379')
 
 # Application definition
 

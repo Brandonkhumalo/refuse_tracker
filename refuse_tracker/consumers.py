32c73b97f8151fd38fe63473dd0e379132b06b54
Brandon <Apex2.0predator@gmail.com>
2025-10-06 15:21:31 +0200

    gg

--- refuse_tracker/consumers.py
+++ refuse_tracker/consumers.py
@@ -2,8 +2,6 @@ import json
 from channels.generic.websocket import AsyncWebsocketConsumer
 from channels.db import database_sync_to_async
 from django.utils import timezone
-from refuse_tracker.models import Truck, LocationUpdate
-from refuse_tracker.tasks import send_truck_proximity_alert
 
 class TruckTrackingConsumer(AsyncWebsocketConsumer):
     async def connect(self):
@@ -18,6 +16,10 @@ class TruckTrackingConsumer(AsyncWebsocketConsumer):
         truck_id = data["truck_id"]
         lat = data["latitude"]
         lng = data["longitude"]
+
+        # Import models and tasks inside function to avoid AppRegistryNotReady
+        from refuse_tracker.models import Truck, LocationUpdate
+        from refuse_tracker.tasks import send_truck_proximity_alert
 
         location = await self.save_location(truck_id, lat, lng)
 
@@ -34,10 +36,13 @@ class TruckTrackingConsumer(AsyncWebsocketConsumer):
             }
         )
 
+        # Trigger proximity alerts
         send_truck_proximity_alert.send(truck_id, lat, lng)
 
     @database_sync_to_async
     def save_location(self, truck_id, lat, lng):
+        # Import models here as well
+        from refuse_tracker.models import Truck, LocationUpdate
         truck = Truck.objects.get(pk=truck_id)
         return LocationUpdate.objects.create(
             truck=truck,
@@ -60,6 +65,9 @@ class ResidentTrackingConsumer(AsyncWebsocketConsumer):
 
         await self.accept()
 
+        # Optionally send last known truck location from DB
+        await self.send_last_truck_location()
+
     async def disconnect(self, close_code):
         await self.channel_layer.group_discard("trucks", self.channel_name)
         if hasattr(self, "suburb_group"):
@@ -68,3 +76,33 @@ class ResidentTrackingConsumer(AsyncWebsocketConsumer):
     async def truck_update(self, event):
         # Residents receive truck updates here
         await self.send(text_data=json.dumps(event))
+
+    async def send_last_truck_location(self):
+        # Delayed import
+        from refuse_tracker.models import LocationUpdate
+
+        # Example: get last known truck update
+        last_location = await self.get_latest_location()
+        if last_location:
+            await self.send(text_data=json.dumps({
+                "type": "truck_update",
+                "truck_id": last_location["truck_id"],
+                "latitude": last_location["latitude"],
+                "longitude": last_location["longitude"],
+                "timestamp": str(last_location["timestamp"]),
+                "truck_name": last_location.get("truck_name", "Truck")
+            }))
+
+    @database_sync_to_async
+    def get_latest_location(self):
+        from refuse_tracker.models import LocationUpdate
+        last = LocationUpdate.objects.order_by("-timestamp").first()
+        if last:
+            return {
+                "truck_id": last.truck.id,
+                "latitude": last.latitude,
+                "longitude": last.longitude,
+                "timestamp": last.timestamp,
+                "truck_name": getattr(last.truck, "name", "Truck")
+            }
+        return None

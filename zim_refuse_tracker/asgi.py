0b7948c48ac08dc8e2535dbd69d1a1c45239d440
Brandon <Apex2.0predator@gmail.com>
2025-10-06 15:16:26 +0200

    jjj

--- zim_refuse_tracker/asgi.py
+++ zim_refuse_tracker/asgi.py
@@ -1,10 +1,12 @@
 import os
-from channels.routing import ProtocolTypeRouter, URLRouter
-from refuse_tracker.middleware import JWTAuthMiddleware
-from django.core.asgi import get_asgi_application
-import refuse_tracker.routing
 
+# Must be set first
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'zim_refuse_tracker.settings')
+
+from django.core.asgi import get_asgi_application
+from channels.routing import ProtocolTypeRouter, URLRouter
+import refuse_tracker.routing
+from refuse_tracker.middleware import JWTAuthMiddleware
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
